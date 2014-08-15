61c18baddf90169a9f15b8008d010043c214b27a
W738806 <d.lake.smith@eagles.usm.edu>
2014-08-15 08:22:28 -0500

    Complete password strategy

--- app.rb
+++ app.rb
@@ -23,6 +23,24 @@ class Application < Sinatra::Base
 		env['REQUEST_METHOD'] = 'POST'
 	end
 
+	Warden::Strategies.add(:password) do
+		def valid?
+			params['user']['username'] && params['user']['password']
+		end
+
+		def authenticate!
+			user = User.first(username: params['user']['username'])
+
+			if user.nil?
+				fail!("The username you entered does not exist.")
+			elseif user.authenticate(params['user']['password'])
+				success!(user)
+			else
+				fail!("Could not log in")
+			end
+		end
+	end
+
 	get '/' do
 		'Hello World!'
 	end

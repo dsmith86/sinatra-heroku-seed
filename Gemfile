66bf0f7243616c2908d988a5853a73fe6f085c3b
W738806 <d.lake.smith@eagles.usm.edu>
2014-08-15 08:04:53 -0500

    Begin adding Warden configuration

--- Gemfile
+++ Gemfile
@@ -3,9 +3,9 @@ source "https://rubygems.org"
 ruby "2.0.0"
 
 gem "sinatra"
-gem "activerecord"
-gem "sinatra-activerecord"
+gem "sinatra-flash"
 gem "data_mapper"
+gem "warden"
 
 gem "pg"
 gem "dm-postgres-adapter"
--- app.rb
+++ app.rb
@@ -1,10 +1,29 @@
-require 'sinatra'
+require 'bundler'
+Bundler.require
 require 'net/http'
 require 'net/https'
 require 'base64'
 require 'json'
 require './config/environments'
 
-get '/' do
-	'Hello, World!'
+class Application < Sinatra::Base
+	enable :sessions
+	register Sinatra::Flash
+
+	use Warden::Manager do |config|
+		config.serialize_into_session {|user| user.id}
+
+		config.serialize_from_session {|id| User.get(id)}
+
+		config.scope_defaults :default, strategies: [:password], action: 'auth/unauthenticated'
+		config.failure_app = self
+	end
+
+	Warden::Manager.before_failure do |env, opts|
+		env['REQUEST_METHOD'] = 'POST'
+	end
+
+	get '/' do
+		'Hello World!'
+	end
 end
